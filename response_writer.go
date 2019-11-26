e0ce5c6341134990d1be0760d594f3f42f0db63e
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-11-26 23:50:43 +0100

    :construction:

--- response_writer.go
+++ response_writer.go
@@ -24,18 +24,22 @@ func NewResponseWriter() *ResponseWriter {
 
 var _ http.ResponseWriter = (*ResponseWriter)(nil)
 
+// Header implements http.ResponseWriter.Header
 func (rw *ResponseWriter) Header() http.Header {
 	return rw.header
 }
 
+// Write implements http.ResponseWriter.Write
 func (rw *ResponseWriter) Write(p []byte) (int, error) {
 	return rw.buf.Write(p)
 }
 
+// WriteHeader implements http.ResponseWriter.WriteHeader
 func (rw *ResponseWriter) WriteHeader(statusCode int) {
 	rw.statusCode = statusCode
 }
 
+// JSResponse builds and returns the equivalent JS Response
 func (rw *ResponseWriter) JSResponse() js.Value {
 	init := js.Global().Get("Object").New()
 
@@ -43,7 +47,9 @@ func (rw *ResponseWriter) JSResponse() js.Value {
 		init.Set("status", rw.statusCode)
 	}
 
-	//FIXME headers
+	if len(rw.header) != 0 {
+		init.Set("headers", map[string][]string(rw.header))
+	}
 
 	jsBody := js.Global().Get("Uint8Array").New(rw.buf.Len())
 	js.CopyBytesToJS(jsBody, rw.buf.Bytes())
