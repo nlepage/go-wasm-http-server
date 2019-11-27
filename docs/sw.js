75a141520e350020cfcbe3f3b29b4e61676e5ec1
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-11-27 13:05:22 +0100

    :alembic:

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 importScripts(
-  'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@latest/lib/wasm_exec/wasm_exec.js',
-  'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@latest/index.js',
+  'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@96677e251874f074906c61ccebd283c63cdec54d/lib/wasm_exec/wasm_exec.js',
+  'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@96677e251874f074906c61ccebd283c63cdec54d/index.js',
 )
 
 console.log('scope', registration.scope)
