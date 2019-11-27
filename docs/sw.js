9dd5381898b6ae374393629c2d7171944e9c3f39
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-11-27 12:46:29 +0100

    :alembic:

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,17 @@
-addEventListener('activate', event => {
-  console.log('activate!')
-  event.waitUntil(clients.claim())
-})
-
 importScripts(
   'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@latest/lib/wasm_exec/wasm_exec.js',
   'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@latest/index.js',
 )
 
-wasmhttp.serve({
-  wasm: 'api.wasm',
-  base: '/api',
+addEventListener('install', event => {
+  console.log('install!')
+  wasmhttp.serve({
+    wasm: 'api.wasm',
+    base: '/api',
+  })
+})
+
+addEventListener('activate', event => {
+  console.log('activate!')
+  event.waitUntil(clients.claim())
 })
