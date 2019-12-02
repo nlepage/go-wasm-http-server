21aa954d1c747b6e556c42eacb5a17478245dd17
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-12-02 17:04:06 +0100

    :alembic:

---
+++ docs/sw.js
@@ -0,0 +1,1 @@
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@b4be701615d284296a4a1aaa852db02f139969ee/index.js')
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const swUrl = 'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@b4be701615d284296a4a1aaa852db02f139969ee/index.js'
-
 window.wasmhttp = {
-  register: async (wasm, { scope, base = '' } = {}) => {
+  register: async (wasm, { scope, base = '', swUrl = 'sw.js' } = {}) => {
     const options = {}
     if (scope) options.scope = scope
     //FIXME register once
