24d9d2d727a9747e177055d9a15c6c8dcc445c59
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-12-02 16:58:20 +0100

    :alembic:

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const swUrl = 'https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@b4be701615d284296a4a1aaa852db02f139969ee/index.js'
+
 window.wasmhttp = {
   register: async (wasm, { scope, base = '' } = {}) => {
     const options = {}
     if (scope) options.scope = scope
     //FIXME register once
-    const registration = await navigator.serviceWorker.register('sw.js', options)
+    const registration = await navigator.serviceWorker.register(swUrl, options)
     await navigator.serviceWorker.ready
     registration.active.postMessage({
       type: 'wasmhttp.register',
