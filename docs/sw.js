3d97d94c35d0e2bb240f391612a308bd6ade5e4e
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-12-02 17:13:12 +0100

    :alembic:

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@d6c4e9ef12bc8ac614ab90e6fe7c4d5db1f868e2/ws.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@d6c4e9ef12bc8ac614ab90e6fe7c4d5db1f868e2/sw.js')
