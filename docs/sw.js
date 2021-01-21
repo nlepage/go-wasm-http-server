2a84a402ce54760604057daa53ca991d43fda347
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-01-21 21:50:35 +0100

    :alembic: Try w/o using jsdelivr

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@254bd5e2ea5b84b4ed2918f8edc7981fb24b06ad/sw.js')
+importScripts('https://raw.githubusercontent.com/nlepage/go-wasm-http-server/master/sw.js')
