9ffd514abcc03c6a4f7e7b5fda3af1baede36608
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-01-21 23:09:20 +0100

    :alembic:

--- docs/sw.js
+++ docs/sw.js
@@ -1,3 +1,3 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@latest/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@51d77eecb21c94236bd144c34789b1974df0248c/sw.js')
 
 registerWasmHTTPListener('api.wasm')
