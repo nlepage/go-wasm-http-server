0bc5d261a5bc6d4da0f24b760698708d93b3e8a6
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-01-21 23:41:43 +0100

    :alembic:

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@9ee7a321b6d6c00036da1c6a47bd5c1b78c011e2/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@48880aadf0163ec0d688dc41f121bd143ab6c305/sw.js')
 
 addEventListener('install', (event) => {
   event.waitUntil(skipWaiting())
