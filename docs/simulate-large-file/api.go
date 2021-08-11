2aa4b38f4000a5bf2a61345073dde43d72774dfd
xiaobai <xiaobai-world@foxmail.com>
2021-08-12 06:48:57 +0800

    improve the code according to pr comments

--- docs/simulate-large-file/api.go
+++ docs/simulate-large-file/api.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"net/http"
 	"strconv"
 
@@ -19,23 +18,30 @@ func main() {
 		vars := req.URL.Query()
 		size, ok := vars["size"]
 		if !ok {
-			bytes.NewBuffer([]byte("url must have size paramter.")).WriteTo(res)
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("URL must have size parameter."))
 			return
 		}
 
-		targetSize, _ := strconv.ParseInt(size[0], 0, 0)
+		targetSize, err := strconv.ParseInt(size[0], 0, 0)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("Size must be 0 or a positive integer."))
+			return
+		}
 
 		if targetSize < 0 || targetSize > 300 {
-			bytes.NewBuffer([]byte("url must gte 0 and lte 300.")).WriteTo(res)
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("Size must be >= 0 and <= 300."))
 			return
 		}
 
 		res.Header().Set("Content-Type", "application/octet-stream")
 		res.Header().Set("Content-Disposition", "attachment;filename=size-file")
 		for i := int64(1); i <= 1024*targetSize; i++ {
-			bytes.NewBuffer([]byte(one_kb)).WriteTo(res)
+			res.Write([]byte(one_kb))
 		}
-		bytes.NewBuffer([]byte("")).WriteTo(res)
 	})
 
 	wasmhttp.Serve(nil)
