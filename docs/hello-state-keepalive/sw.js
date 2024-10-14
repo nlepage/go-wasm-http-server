770d49a106f1bc7afc33e3eda689edce904be634
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2024-10-14 23:29:31 +0200

    chore: v2.0.2

--- docs/hello-state-keepalive/sw.js
+++ docs/hello-state-keepalive/sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.jsdelivr.net/gh/golang/go@go1.23.1/misc/wasm/wasm_exec.js')
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@v2.0.1/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@v2.0.2/sw.js')
 
 addEventListener('install', event => {
   event.waitUntil(skipWaiting())
