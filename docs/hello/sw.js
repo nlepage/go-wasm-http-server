2f3e39bf9791c6c1b5442682ba765aa9cb5b2bc9
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-02-06 11:21:25 +0100

    chore: v2.2.0

--- docs/hello/sw.js
+++ docs/hello/sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.jsdelivr.net/gh/golang/go@go1.23.1/misc/wasm/wasm_exec.js')
-importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@v2.1.0/sw.js')
+importScripts('https://cdn.jsdelivr.net/gh/nlepage/go-wasm-http-server@v2.2.0/sw.js')
 
 const wasm = 'api.wasm'
 
